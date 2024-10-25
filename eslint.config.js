9a1f80e7c31d7778ef0e28827ef99112590d6325
yanghyeonjin <guswls7081@gmail.com>
2024-10-25 17:03:41 +0900

    feat: eslint 추가

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import pluginVue from 'eslint-plugin-vue';
+import pluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  ...pluginVue.configs['flat/recommended'],
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          useTabs: false,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 120,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+];
